5ff39ed4cffbd69ecb865150d44501ed3b59f4dd
Jaeho Cha <ischar@naver.com>
2024-08-18 13:22:37 +0900

    Add: Initial project setting

---
+++ backend/src/main/java/ischar/indrama/IndramaApplication.java
@@ -0,0 +1,13 @@
+package ischar.indrama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndramaApplication.class, args);
+	}
+
+}
